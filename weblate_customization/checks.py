bcb6a9b15b2756255817ced8db5580f78e782fb2
joe <burtonjae@hotmail.co.uk>
2024-12-15 22:18:01 +0000

    Improve error messages of PercentSOnlyCheck

--- weblate_customization/checks.py
+++ weblate_customization/checks.py
@@ -1,5 +1,7 @@
 
 import re
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 from weblate.checks.base import TargetCheck
 from weblate.checks.format import JAVA_MATCH
 
@@ -18,11 +20,31 @@ class LegacyFormattingCodeCheck(TargetCheck):
 class PercentSOnlyCheck(TargetCheck):
     check_id = 'percent_s_only'
     name = 'Percent S only'
-    description = 'Translation contains a formatting code which is not "%s" or "%n$s"'
+    description = 'Translation contains a format specifier which is not "%s" or "%n$s"'
     default_disabled = True
 
     def check_single(self, source, target, unit):
-        return not all((ALLOWED_FORMATTING_CODES.fullmatch(x.group()) for x in JAVA_MATCH.finditer(target.replace('%%', ''))))
+        return any(self.get_illegal_format_codes(target))
+    
+    def get_description(self, check_obj):
+        unit = check_obj.unit
+        targets = unit.get_target_plurals()
+        if len(targets) != 1:
+            return super().get_description(check_obj)
+        
+        errors = [
+            f'Translation contains illegal format specifier "{code}", use "%s" or "%n$s" only' 
+            for code in set(self.get_illegal_format_codes(targets[0]))
+        ]
+        if len(errors) == 0:
+            return super().get_description(check_obj)
+        
+        return format_html_join(mark_safe("<br />"), "{}", ((error,) for error in errors))
+    
+    def get_illegal_format_codes(target):
+        for m in JAVA_MATCH.finditer(target.replace('%%', '')):
+            if not ALLOWED_FORMATTING_CODES.fullmatch(m.group()):
+                yield m.group()
 
 class PercentUnescapedCheck(TargetCheck):
     check_id = 'percent_unescaped'
